643294335b4e01b3637af994068bb94ab23b5a75
xxyeon <jykim9335@gmail.com>
2024-03-21 19:23:57 +0900

    feat: DocFile 엔티티 추가

--- src/main/java/smw/capstone/entity/Board.java
+++ src/main/java/smw/capstone/entity/Board.java
@@ -23,4 +23,6 @@ public class Board {
 
     private LocalDate updateAt;
 
+    private String content;
+
 }
---
+++ src/main/java/smw/capstone/entity/DocFile.java
@@ -0,0 +1,18 @@
+package smw.capstone.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class DocFile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JoinColumn(name = "Dcouments_Id")
+    @ManyToOne
+    private Documents document;
+
+    @JoinColumn(name = "Id")
+    @ManyToOne
+    private Files file;
+}
