549df1196ef74ec1711caef8cfa9208b1e9e320e
jieon419 <leejiun0419@naver.com>
2024-04-05 19:28:17 +0900

    회원가입 페이지

--- frontend/src/component/member/CreateAccountEmail.jsx
+++ frontend/src/component/member/CreateAccountEmail.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
+import { defaultInstance } from '../../util/api';
 
 function CreateAccountEmail() {
+    const url = "/member/save"
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData);
+        try {
+          await defaultInstance.post(url, data).then((res) => {
+            if (!res.ok) {
+              throw new Error("error");
+            }
+          });
+        } catch (error) {
+        }
+    }
     return (
         <div style={{ textAlign: 'left' }}>
             <h2><b>계정 만들기</b></h2>
-            <label htmlFor="email">이메일</label><br />
-            <input type="email" id="email" name="email" /><br /><br />
-            <p>이메일 허용 목록이 활성화 되어 있습니다. 이메일 허용 목록에 존재하는 메일만 사용할 수 있습니다.</p>
-            <ul style={{ listStyleType: 'disc' }}>
-                <li>naver.com</li>
-                <li>gmail.com</li>
-                <li>kakao.com</li>
-            </ul>
+            <form id="form" onSubmit={handleSubmit}>
+                <label htmlFor="email">이메일</label><br />
+                <input type="email" id="email" name="email" /><br /><br />
+                <p>이메일 허용 목록이 활성화 되어 있습니다. 이메일 허용 목록에 존재하는 메일만 사용할 수 있습니다.</p>
+                <ul style={{ listStyleType: 'disc' }}>
+                    <li>sangmyung.kr</li>
+                </ul>
+            </form>
             <p><b>가입후 탈퇴는 불가능합니다.</b></p>
             <div>
                 <button type="submit">
--- frontend/src/component/member/CreateAccoutId.jsx
+++ frontend/src/component/member/CreateAccoutId.jsx
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
+import { defaultInstance } from '../../util/api';
 
 const CreateAccountId = () => {
+  const url = "/member/save";
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    try {
+      await defaultInstance.post(url, data).then((res) => {
+        if (!res.ok) {
+          throw new Error("error");
+        }
+      });
+    } catch (error) {
+    }
+  }
   const [userId, setUserId] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
@@ -20,40 +36,43 @@ const CreateAccountId = () => {
     setConfirmPassword(event.target.value);
   };
 
+
   return (
     <div>
       <h2>계정 만들기</h2>
-      <div>
-        <label htmlFor="userIdInput">사용자 ID</label><br />
-        <input
-          type="text"
-          id="userIdInput"
-          value={userId}
-          onChange={handleUserIdChange}
-        />
+      <form id="form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="userIdInput">사용자 ID</label><br />
+          <input
+            type="text"
+            id="userIdInput"
+            value={userId}
+            onChange={handleUserIdChange}
+          />
+        </div>
+        <button>중복확인</button>
+        <div>
+          <label htmlFor="passwordInput">암호</label><br />
+          <input
+            type="password"
+            id="passwordInput"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="confirmPasswordInput">암호 확인</label><br />
+          <input
+            type="password"
+            id="confirmPasswordInput"
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+          />
+        </div>
+      </form>
+        <p>가입 후 탈퇴는 불가능합니다.</p>
+        <button><NavLink to="/confirmEmail">가입</NavLink></button>
       </div>
-      <button>중복확인</button>
-      <div>
-        <label htmlFor="passwordInput">암호</label><br />
-        <input
-          type="password"
-          id="passwordInput"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="confirmPasswordInput">암호 확인</label><br />
-        <input
-          type="password"
-          id="confirmPasswordInput"
-          value={confirmPassword}
-          onChange={handleConfirmPasswordChange}
-        />
-      </div>
-      <p>가입 후 탈퇴는 불가능합니다.</p>
-      <button><NavLink to="/confirmEmail">가입</NavLink></button>
-    </div>
   );
 };
 
