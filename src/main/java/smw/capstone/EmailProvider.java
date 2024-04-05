b830784a96467a1e392e85616c04f5a987b88b99
rilac <asd990702@naver.com>
2024-04-05 21:00:18 +0900

    이메일 작업중

--- src/main/java/smw/capstone/EmailProvider.java
+++ src/main/java/smw/capstone/EmailProvider.java
@@ -1,10 +1,11 @@
 package smw.capstone;
 
-import jakarta.mail.internet.MimeMessage;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import javax.mail.internet.MimeMessage;
 
 @Component
 @RequiredArgsConstructor
