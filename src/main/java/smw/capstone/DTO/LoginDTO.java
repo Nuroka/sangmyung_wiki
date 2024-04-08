b31fa174dbd8290389256af3395ef9265ac1388d
rilac <eodos6480@gmail.com>
2024-04-08 17:54:01 +0900

    DTO 수정, 로그인 구현

--- src/main/java/smw/capstone/DTO/LoginDTO.java
+++ src/main/java/smw/capstone/DTO/LoginDTO.java
@@ -1,12 +1,15 @@
 package smw.capstone.DTO;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class LoginDTO {
     @NotNull
+    @JsonProperty("username")
     private String Username;
     @NotNull
+    @JsonProperty("password")
     private String Password;
 }
