6aaf44afd32936faf7e76d177d2c3252a5e1f78e
Jung Hoon <52675944+JH627@users.noreply.github.com>
2024-05-12 19:14:22 +0900

    마이페이지 수정
    오류 알림 추가, react-query => useEffect로 변경

--- frontend/src/component/member/MyPageContent.jsx
+++ frontend/src/component/member/MyPageContent.jsx
@@ -1,57 +1,63 @@
-import { useQuery } from "@tanstack/react-query";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { authInstance } from "../../util/api";
-import { useNavigate } from "react-router-dom";
-import styles from "./Login.module.css";
+import styles from "../Login.module.css";
 import findIdAuthStyles from "./FindIdForm.module.css";
 
 export default function MyPageContent() {
   const url = "/mypage";
 
-  const navigate = useNavigate();
+  const [data, setData] = useState();
+  const [error, setError] = useState();
 
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["/mypage"],
-    queryFn: async () => {
-      const response = await authInstance.post(url);
-      // Http Status Error handling
-      return response.data;
-    },
-    select: (res) => {
-      return {
-        username: res.username,
-        email: res.email,
-        adminType: res.adminType,
-        // skin
-      };
-    },
-  });
+  useEffect(() => {
+    async function fetchData() {
+      setError();
+      authInstance
+        .post(url)
+        .then(function (res) {
+          if (res.status === 200) {
+            setData(res.data);
+          } else {
+            throw new Error();
+          }
+        })
+        .catch(function (e) {
+          setError({ message: "정보 가져오기 실패! 다시 시도해주세요." });
+        });
+    }
+    fetchData();
+  }, []);
+
+  const navigate = useNavigate();
 
   return (
     <>
-      {isLoading ? (
+      {!data ? (
         <p>로딩 중...</p>
       ) : (
         <>
-          {isError && <p>{error.message}</p>}
-          <h2 className={styles.loginTitle}>내 정보</h2>
-
-          <p>사용자 이름</p>
-          <p>{data.username}</p>
-          <p>이메일</p>
-          <p>{data.email}</p>
-          <p>이메일 변경</p>
-          <p>권한</p>
-          <p>{data.adminType}</p>
-          <p>비밀번호</p>
-          <button className={`${styles.link} ${findIdAuthStyles.findIdFormBtn}`} onClick={() => navigate("/member/update")}>
-            비밀번호 변경
-          </button>
-          <p>스킨</p>
-          <select id="skin" name="skin">
-            <option value="normal">밝은 테마</option>
-            <option value="black">어두운 테마</option>
-          </select>
+          {error ? (
+            <p>{error.message}</p>
+          ) : (
+            <>
+              <h2 className={styles.loginTitle}>내 정보</h2>
+              <p>사용자 이름</p>
+              <p>{data.username}</p>
+              <p>이메일</p>
+              <p>{data.email}</p>
+              <p>
+                비밀번호
+                <button
+                  className={`${styles.link} ${findIdAuthStyles.findIdFormBtn}`}
+                  onClick={() => navigate("/member/update")}
+                >
+                  비밀번호 변경
+                </button>
+              </p>
+            </>
+          )}
         </>
       )}
     </>
