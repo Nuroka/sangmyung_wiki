fa190bc278d92cadc4ff70b46fa0e90c2367f5d0
Jung Hoon <52675944+JH627@users.noreply.github.com>
2024-06-18 16:57:58 +0900

    임시 문서 역사 페이지 생성
    1. 통신 추가 필요

--- frontend/src/component/docs/DocsLog.jsx
+++ frontend/src/component/docs/DocsLog.jsx
@@ -1,90 +1,20 @@
-import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { defaultInstance } from "../../util/api";
-import styles from "../docs/RecentEditedLog.module.css";
 
-const DocsLog = () => {
-  let url = "/docs/log";
-
-  const initialDocsLogData = [
-    {
-      id: 1,
-      date: "2024-02-22",
-      time: "19:28:14",
-      changes: "r003",
-      editor: "User3",
-    },
-    {
-      id: 2,
-      date: "2024-02-21",
-      time: "14:25:14",
-      changes: "r002",
-      editor: "User2",
-    },
-    {
-      id: 3,
-      date: "2024-02-19",
-      time: "12:25:13",
-      changes: "r001",
-      editor: "User1",
-    },
-  ];
-
-  const [docsLogData, setDocsLogData] = useState(initialDocsLogData);
-  const [currentIndex, setCurrentIndex] = useState(0);
+export default function DocsLog({ data }) {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const goToPrevPage = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex(currentIndex - 1);
-    }
-  };
-
-  const goToNextPage = () => {
-    if (currentIndex < docsLogData.length - 1) {
-      setCurrentIndex(currentIndex + 1);
-    }
-  };
-
   const goToDocument = (id) => {
-    navigate(`/document/${id}`);
-  };
-
-  const fetchData = async () => {
-    try {
-      const response = await defaultInstance.get(url);
-      setDocsLogData(response.data);
-    } catch (error) {
-      console.error("Error");
-    }
+    navigate("/doc");
   };
 
   return (
     <div>
-      <h1 className={styles.recentTitle}>문서 역사</h1>
-      <div>
-        <button className={styles.whiteBtn} onClick={goToPrevPage}>
-          {"< Prev"}
-        </button>
-        <button className={styles.whiteBtn} onClick={goToNextPage}>
-          {"Next >"}
-        </button>
-      </div>
-      <div>
-        {initialDocsLogData.map((log, index) => (
-          <p key={index}>
-            &bull; {log.date} {log.time} (
-            <span onClick={() => goToDocument(log.id)}>보기</span> | ){" "}
-            {log.changes} {log.editor}
-          </p>
-        ))}
-      </div>
+      {data.map((log, index) => (
+        <p key={index}>
+          &bull; {log.update_at} {log.title} {log.log} (
+          <span onClick={() => goToDocument(log.docs_id)}>보기</span>)
+        </p>
+      ))}
     </div>
   );
-};
-
-export default DocsLog;
+}
--- frontend/src/pages/docs/DocsLog.jsx
+++ frontend/src/pages/docs/DocsLog.jsx
@@ -1,9 +1,78 @@
+import { useState, useEffect } from "react";
+
+import outlet from "../../layout/OutletLayout.module.css";
+
+import { defaultInstance } from "../../util/api";
 import DocsLog from "../../component/docs/DocsLog";
+import PaginateBox from "../../component/PaginateBox";
 
 export default function DocsLogPage() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [logsPerPage] = useState(2);
+
+  const url = "/docs/log";
+
+  const testData = [
+    {
+      docs_id: 1,
+      update_at: "2024-02-22",
+      title: "title",
+      log: "log",
+    },
+    {
+      docs_id: 2,
+      update_at: "2024-02-21",
+      title: "title",
+      log: "log2",
+    },
+    {
+      docs_id: 3,
+      update_at: "2024-02-20",
+      title: "title",
+      log: "log3",
+    },
+  ];
+
+  useEffect(() => {
+    async function fetchLogs() {
+      // setError(null);
+      // defaultInstance
+      //   .get(url)
+      //   .then(function (res) {
+      //     if (res.status === 200) {
+      setData(testData);
+      //   } else {
+      //     throw new Error();
+      //   }
+      // })
+      // .catch(function (e) {
+      //   setError({ message: "정보 가져오기 실패! 다시 시도해주세요." });
+      // });
+    }
+    fetchLogs();
+  }, []);
+
+  const indexOfLastLog = currentPage * logsPerPage;
+  const indexOfFirstLog = indexOfLastLog - logsPerPage;
+  const currentLogs = data.slice(indexOfFirstLog, indexOfLastLog);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const totalPages = Math.ceil(data.length / logsPerPage);
+
   return (
     <>
-      <DocsLog />
+      <h2 className={outlet.title}>문서 역사</h2>
+      <br />
+      <PaginateBox
+        currentPage={currentPage}
+        totalPages={totalPages}
+        paginate={paginate}
+      />
+      <br />
+      {error && <p>{error.message}</p>}
+      {data && <DocsLog data={currentLogs} />}
     </>
   );
 }
