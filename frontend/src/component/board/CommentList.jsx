74962691127ad3c305622b0c71780726d0e80711
Nuroka <inhyukjeong@gmail.com>
2024-06-23 05:58:24 +0900

    댓글 로직 변경(실행X)

--- frontend/src/component/board/CommentList.jsx
+++ frontend/src/component/board/CommentList.jsx
@@ -1,50 +1,73 @@
-import React, { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+import Pagination from "react-js-pagination";
+
+import Comment from "./Comment.js"
 import { authInstance } from "../../util/api";
-import AddComment from "./AddComment";
-import EditComment from "./EditComment";
-import DeleteComment from "./DeleteComment";
-import { useSearchParams } from "react-router-dom";
-import CommentStyles from "./Comment.module.css";
 
-const CommentList = ({ boardId }) => {
-  const [comments, setComments] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [searchParams, setSearchParams] = useSearchParams();
+function CommentList(props) {
 
-  useEffect(() => {
-    fetchComments();
-  }, []);
+	const seq = props.seq;
 
-  const fetchComments = async () => {
-    try {
-      const response = await (await authInstance.get(`/comments/board/one`,{params:{ idx: searchParams.get("id")},}));
-      setComments(response.data);
-      setLoading(false);
-    } catch (error) {
-      console.error("실패", error);
-    }
-  };
+	// Paging
+	const [page, setPage] = useState(1);
+	const [totalCnt, setTotalCnt] = useState(0);
 
+	const [commentList, setCommentList] = useState([]);
 
+	const changePage = (page) => {
+		setPage(page);
+		getCommentList(page);
+	}
 
+	const getCommentList = async (page) => {
+		await authInstance.get(`/comment`, { params: { "bbsSeq": seq, "page": page } })
+			.then((resp) => {
+				console.log("[BbsComment.js] getCommentList() success :D");
+				console.log(resp.data);
 
-  return (
-    <div>
-      <h3>Comments</h3>
-      <AddComment boardId={boardId} />
-      {loading ? (
-        <p>Loading comments...</p>
-      ) : (
-        comments.map((comment) => (
-          <div className={CommentStyles.editCommentBtn} key={comment.comment_id}>
-            {/*<p>{comment.content}</p>*/}
-            <DeleteComment commentId={comment.comment_id} />
-            <EditComment commentId={comment.comment_id} initialContent={comment.content} />
-          </div>
-        ))
-      )}
-    </div>
-  );
-};
+				setCommentList(resp.data.commentList);
+				setTotalCnt(resp.data.pageCnt);
+
+			}).catch((err) => {
+				console.log("[BbsComment.js] getCommentList() error :<");
+				console.log(err);
+
+			});
+	}
+
+	useEffect(() => {
+		getCommentList(1);
+	}, []);
+
+	return (
+		<>
+
+			<div className="my-1 d-flex justify-content-center">
+				<h5><i className="fas fa-paperclip"></i> 댓글 목록 </h5>
+			</div>
+
+			<Pagination
+				activePage={page}
+				itemsCountPerPage={5}
+				totalItemsCount={totalCnt}
+				pageRangeDisplayed={5}
+				prevPageText={"‹"}
+				nextPageText={"›"}
+				onChange={changePage} />
+			{
+				commentList.map(function (comment, idx) {
+					return (
+						<div className="my-5" key={idx}>
+							<Comment obj={comment} key={idx} />
+						</div>
+					);
+				})
+			}
+
+		</>
+
+	);
+}
+
 
 export default CommentList;
