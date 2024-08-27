ae9c570cf6102d0aebe78bfd40edccebae445f07
Jung Hoon <52675944+JH627@users.noreply.github.com>
2024-08-28 01:51:55 +0900

    Add: 계정생성, 아이디/비밀번호 찾기 결과창 로그인화면 이동버튼 추가

--- frontend/src/component/member/CreateAccountResult.jsx
+++ frontend/src/component/member/CreateAccountResult.jsx
@@ -1,11 +1,17 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+import styles from "../Login.module.css";
 
 export default function CreateAccountResult({ username }) {
+  const navigate = useNavigate();
+
   return (
     <div>
       <p>환영합니다! {username}님의 계정 생성이 완료되었습니다.</p>
       <br />
-      <Link to="/user">로그인</Link>
+      <button className={styles.link} onClick={() => navigate("/user")}>
+        로그인
+      </button>
     </div>
   );
 }
--- frontend/src/component/member/FindIdResult.jsx
+++ frontend/src/component/member/FindIdResult.jsx
@@ -1,10 +1,19 @@
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+import styles from "../Login.module.css";
+
 export default function FindIdResult({ username }) {
+  const navigate = useNavigate();
+
   return (
     <>
       <p>인증 완료!</p>
+      <br />
       <p>{username}</p>
-      <NavLink to="/user">로그인</NavLink>
+      <br />
+      <button className={styles.link} onClick={() => navigate("/user")}>
+        로그인
+      </button>
     </>
   );
 }
